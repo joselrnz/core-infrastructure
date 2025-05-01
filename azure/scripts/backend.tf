7bb70f3fd1b5b36c720d5d2eeb72240ebbec3626
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-01 13:11:34 -0500

    update

--- azure/scripts/backend.tf
+++ azure/scripts/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
+    resource_group_name  = "data-dp203"
     storage_account_name = "terraformstateclouddev"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"

fbc3840f387984f644523d1bc2a32ab84695aea0
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-01 15:51:07 -0500

    update

--- azure/scripts/variables.tf
+++ azure/scripts/variables.tf
@@ -92,7 +92,7 @@ variable "containers" {
   default = []
 }
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+# variable "client_id" {}
+# variable "client_secret" {}
+# variable "tenant_id" {}
+# variable "subscription_id" {}

6f14d0491dab4018b7b6f983bee6fe6a9a0c5d54
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-06 21:39:12 -0500

    update

--- digitalocean/scripts/provider.tf
+++ digitalocean/scripts/provider.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = ">= 2.24"
     }
   }
 }
-
 provider "digitalocean" {
   token = var.do_token
 }

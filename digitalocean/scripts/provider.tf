6007bc8be8dc81fc43441cc8a4e5e28789fcf4d9
Jose Lorenzo <joselrnz19@gmail.com>
2025-05-06 20:53:55 -0500

    update

--- digitalocean/scripts/provider.tf
+++ digitalocean/scripts/provider.tf
@@ -1,3 +1,21 @@
 provider "digitalocean" {
   token = var.do_token
 }
+
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+  }
+}
